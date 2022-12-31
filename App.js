cce6fccc589b9453c3f972a1bf89bb6673a170e7
Aaron <aarongrossman.dev@gmail.com>
2022-12-30 22:34:56 -0500

    ubereats2.0 w/ amplify

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, View, SafeAreaView } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
-import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
-import DishDetailsScreen from './src/screens/DishDetailsScreen';
-import CartScreen from './src/screens/CartScreen';
-import OrdersScreen from './src/screens/OrdersScreen';
-import OrderDetails from './src/screens/OrderDetails';
-
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
 
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native'
+import awsconfig from './src/aws-exports';
+// import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig);
 
 
 
-export default function App() {
+
+function App() {
   return (
     <NavigationContainer>
-      
-        <RootNavigator />
+      <RootNavigator />
         <StatusBar style="light" />
         
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
